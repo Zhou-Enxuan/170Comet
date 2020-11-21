456121390b4d1ffef0cf1e4e15c3356cf5d893d8
Jia Wei <jwei22@ucsc.com>
2020-11-20 19:41:46 -0800

    dialog

---
+++ Scripts/Dialog.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Dialog : MonoBehaviour
+{
+    public GameObject enterDialog;
+
+    private void OnTriggerEnter2D(Collider2D collision){
+        if(collision.tag == "Player"){
+            enterDialog.SetActive(true);
+        }
+    }
+}
