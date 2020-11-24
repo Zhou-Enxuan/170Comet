cd6fe1f16f8085b304deceb74e1fb0ec69c8947f
许华真 <xuhuazhen@xuhuazhendeMacBook-Pro.local>
2020-11-23 20:42:57 -0800

    throw snowball

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // 可以在controller看到的变量
-    [SerializeField]private Rigidbody2D rb;
-    public float speed;
+    [SerializeField]
+    private Rigidbody2D rb;
+    //public float speed;
+    public float move_speed;
+    private bool facing_right = true;
+    private float move_x;
+
     public float jumpforce;
 
 
@@ -16,29 +21,74 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        Movement();
-        // char jump
+    void Update(){
+
+        player_move();
         if(Input.GetButtonDown("Jump")/*&& coll.IsTouchingLayers(ground)*/){
             rb.velocity = new Vector2(rb.velocity.x, jumpforce);
-            // anim.SetBool("jumping", true); 
-        }
-    }
-
-    void Movement(){
-        float horizontalmove = Input.GetAxis("Horizontal");
-        float facedirection = Input.GetAxisRaw("Horizontal");
-
-        // char move
-        if(horizontalmove != 0){
-            rb.velocity = new Vector2(horizontalmove*speed*Time.deltaTime, rb.velocity.y);
-        }
-
-        if(facedirection != 0){
-            transform.localScale = new Vector3(2*facedirection, 2, 1);
+             // anim.SetBool("jumping", true); 
         }
 
     }
+
+    //functions used for all the player actions
+    void player_move(){
+
+        //control
+        move_x = Input.GetAxisRaw("Horizontal");
+
+        //animation
+
+        //player direction
+        if(move_x > 0.0f && facing_right == false){
+
+            flip_player();
+
+        }
+        else if(move_x < 0.0f && facing_right == true){
+
+            flip_player();
+
+        }
+        //physics
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2 (move_x * move_speed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+
+
+    }
+
+    //function used to flip the character sprite alone the x axis
+    void flip_player(){
+
+        facing_right = !facing_right;
+        Vector2 local_scale = gameObject.transform.localScale;
+        local_scale.x *= -1;
+        transform.localScale = local_scale;
+
+    }
+
+    // Update is called once per frame
+    // void FixedUpdate()
+    // {
+    //     Movement();
+    //     // char jump
+    //     if(Input.GetButtonDown("Jump")/*&& coll.IsTouchingLayers(ground)*/){
+    //         rb.velocity = new Vector2(rb.velocity.x, jumpforce);
+    //         // anim.SetBool("jumping", true); 
+    //     }
+    // }
+
+    // void Movement(){
+    //     float horizontalmove = Input.GetAxis("Horizontal");
+    //     float facedirection = Input.GetAxisRaw("Horizontal");
+
+    //     // char move
+    //     if(horizontalmove != 0){
+    //         rb.velocity = new Vector2(horizontalmove*speed*Time.deltaTime, rb.velocity.y);
+    //     }
+
+    //     if(facedirection != 0){
+    //         transform.localScale = new Vector3(2*facedirection, 2, 1);
+    //     }
+
+    // }
 }
---
+++ Assets/snowball.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class snowball : MonoBehaviour
+{
+    public GameObject Snowball;
+	public Transform hold_pos;
+	public float LaunchForce;
+
+	Vector2 Direction;
+	public float force;
+	public GameObject pointPrefab;
+	public GameObject[] points;
+	public int points_number;
+	//bool ReadyToShoot = false;
+	//Vector2 ShootPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+	        points = new GameObject[points_number];
+
+		    for(int i = 0; i < points_number; i++)
+		         {
+		         	points[i] = Instantiate(pointPrefab, hold_pos.position, Quaternion.identity);
+		         } 	
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+        //  	if (!ReadyToShoot)
+        //  	{
+      //   		ShootPos = hold_pos.position;
+
+	         	// points = new GameObject[points_number];
+
+		         // for(int i = 0; i < points_number; i++)
+		         // {
+		         // 	points[i] = Instantiate(pointPrefab, hold_pos.position, Quaternion.identity);
+		         // }
+
+		     // 	ReadyToShoot = true;
+		     // } 
+		     // else 
+		     // {
+		    	Shoot();
+		    // 	ReadyToShoot = false;
+		    // 	Debug.Log(ReadyToShoot);
+		    // }
+
+        }
+
+        Vector2 MousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 holdpos = hold_pos.position;
+
+        // if (Input.GetKeyDown(KeyCode.A) && ReadyToShoot)
+        // {
+        // 	ShootPos = new Vector2(ShootPos.x- 0.5f, ShootPos.y);
+        // }
+        // if (Input.GetKeyDown(KeyCode.D) && ReadyToShoot)
+        // {
+        // 	ShootPos = new Vector2(ShootPos.x+ 0.5f, ShootPos.y);
+        // }
+
+		Direction = MousePos - holdpos;
+		//Direction = ShootPos - holdpos;
+
+		// if (!ReadyToShoot)
+  //       {
+	         faceMouse();
+
+	         for (int i = 0; i < points.Length; i++)
+	         {
+	        	points[i].transform.position = PointPosition(i * 0.1f);
+	         }
+	 //    }
+
+	 //    Debug.Log(ReadyToShoot);
+    }
+
+    void Shoot()
+    {
+    	GameObject SnowballIns = Instantiate(Snowball, hold_pos.position, hold_pos.rotation);
+    	SnowballIns.GetComponent<Rigidbody2D>().velocity = hold_pos.right * LaunchForce;
+    }
+
+    void faceMouse()
+    {
+    	hold_pos.right = Direction;
+    }
+    
+    Vector2 PointPosition(float t)
+    {
+    	Vector2 current_pointsPos = (Vector2)hold_pos.position + (Direction.normalized * force * t) + 0.5f * Physics2D.gravity * (t*t);
+    	return current_pointsPos;
+    }
+	
+}
