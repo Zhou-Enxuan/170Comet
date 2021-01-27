e5370b5964c82f780a6e7fb027e2c21979d9aeb3
Rui Chen <rchen47@ucsc.edu>
2021-01-27 10:20:46 +0800

    reset music game

--- Assets/Script/Level2/RhythmGame/RhythmScore.cs
+++ Assets/Script/Level2/RhythmGame/RhythmScore.cs
@@ -5,45 +5,51 @@ using UnityEngine;
 public class RhythmScore : MonoBehaviour
 {
 	//score setting
-    public static int currentScore = 0;
+    public static int currentScore;
     private static int scorePerNote = 100;
-    public static int totalScore = 0;
+    public static int totalScore;
     public int totalS = 0;
     public bool IsGameEnded = false;
+    public bool PlayAgain;
 
     public static GameObject RhythmGame;
     public static GameObject SadFace;
 
-    void Start() {
+    void OnEnable() {
         RhythmGame = GameObject.FindGameObjectWithTag("RhythmGame");
         SadFace = GameObject.FindGameObjectWithTag("SadFace");
+        currentScore = 0;
+        totalScore = 0;
+        PlayAgain = false;
     }
 
     void Update() {
     	totalS = totalScore;
+<<<<<<< Updated upstream
         //过关 - 得到500分
         
 	    if (currentScore >= 200 && totalScore >= 500) {
+=======
+
+        //过关 - 得到200分
+	    if (currentScore >= 200 && totalScore == 500) {
+>>>>>>> Stashed changes
 	        //stop rhythm game & 五线谱消失
 	        RhythmGame.SetActive(false); 
 	        //continue controlling the biraad
 	        GameObject.Find("Player").GetComponent<Rigidbody2D>().isKinematic = false;
 	        GameObject.Find("Player").GetComponent<PlayerMovement>().enabled = true;
             IsGameEnded = true;
+<<<<<<< Updated upstream
             Debug.Log("过关");
 	        //SadFace.SetActive(true);
 	        //SadFace.GetComponent<CallStaff>().enabled = true;
+=======
+>>>>>>> Stashed changes
 	    }
-	    //没过关 - 未得到500分
+	    //没过关 - 未得到200分
 	    else if (currentScore < 200 && totalScore == 500) {
-	        //stop rhythm game & 五线谱消失
-	        RhythmGame.SetActive(false);
-	        //continue controlling the bird
-	        GameObject.Find("Player").GetComponent<Rigidbody2D>().isKinematic = false;
-	        GameObject.Find("Player").GetComponent<PlayerMovement>().enabled = true;
-	        SadFace.SetActive(true);
-            //SadFace.GetComponent<CallStaff>().enabled = true;
-            //Debug.Log("!!!!!!");
+            PlayAgain = true;
 	    }
         
         
@@ -51,7 +57,7 @@ public class RhythmScore : MonoBehaviour
 
     public static void NoteHit() {
         currentScore += scorePerNote;
-        totalScore += scorePerNote;
+        //totalScore += scorePerNote;
     }
 
     public static void NotePass() {
