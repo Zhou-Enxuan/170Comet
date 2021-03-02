ceb9ba1a2cab195cff36d471ab61a3cd7b604339
Zhou-Enxuan <enzhou64@gmail.com>
2021-03-01 21:20:04 -0800

    update bird animation after news paper

---
+++ Assets/Script/Level2/SummerNews/SummerWinAction.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SummerWinAction : MonoBehaviour
+{
+    [SerializeField]
+    private Transform destination;
+
+    [SerializeField]
+    private float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameManager.instance.stopMoving = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y < destination.position.y)
+        {
+            transform.position += new Vector3(0, speed / 1000, 0);
+        }
+        else
+        {
+            GameManager.instance.stopMoving = false;
+            Debug.Log(transform.position + ": " + destination.position);
+        }
+    }
+}
