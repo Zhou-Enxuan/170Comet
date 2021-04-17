f6006aad2af00338475da200018b2507b45eb65e
Rui Chen <rchen47@ucsc.edu>
2021-04-17 22:45:47 +0800

    Level4Part2 Created

---
+++ Assets/Script/Level4/Part2/DrawingTrigger.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawingTrigger : MonoBehaviour
+{
+    private Animator Anim;
+    public float num;
+    private GameObject Soldier;
+
+    void Awake()
+    {
+        Anim = GetComponent<Animator>();
+        num = Random.Range(0f, 4.2f);
+        Soldier = GameObject.Find("Soldier");
+    }
+
+    void Start()
+    {
+        Anim.enabled = false;
+    }
+
+    void Update()
+    {
+        CloseEyes();
+    }
+
+    private void CloseEyes()
+    {
+        if (Soldier.transform.position.x > num)
+        {
+            Anim.enabled = true;
+            StartCoroutine(WaitanimDone());
+            num = num + Random.Range(5.0f, 10.0f);
+        }
+    }
+
+    IEnumerator WaitanimDone()
+    {
+        yield return new WaitForSeconds(5f);
+        Anim.enabled = false;
+        GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("temp/bird02");
+    }
+}
