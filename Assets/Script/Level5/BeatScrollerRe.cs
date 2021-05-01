954fea7c0b1b44bdfa99d8abb927f04c6d151c42
Rui Chen <rchen47@ucsc.edu>
2021-05-02 00:11:49 +0800

    move RhythmGame to UI

---
+++ Assets/Script/Level5/BeatScrollerRe.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeatScrollerRe : MonoBehaviour
+{
+    [SerializeField] float beatTempo;
+
+    void Start() 
+    {
+
+    }
+
+    void Update() 
+    {
+        transform.position -= new Vector3(beatTempo * Time.deltaTime *2, 0f, 0f);
+    }
+}
---
+++ Assets/Script/Level5/NoteActions.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteActions : MonoBehaviour
+{
+    [SerializeField] MusicButtonController musicButtonController;
+    [SerializeField] float thisIndex;
+    private GameObject Button;
+    [SerializeField] RectTransform rt;
+
+    void Awake()
+    {
+        Button = GameObject.Find("ControlButton");
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (musicButtonController.index == thisIndex)
+        {
+            if (transform.position.x < (Button.transform.position.x + rt.rect.width/2) && transform.position.x > (Button.transform.position.x - rt.rect.width/2))
+            {
+                if(Input.GetKeyDown("space"))
+                {
+                    this.gameObject.SetActive(false);
+                }
+            }
+        }
+    }
+}
