55bda7ec69e6c08dcb32fe6bcbad96bfbf4dfa05
Rui Chen <rchen47@ucsc.edu>
2021-05-08 20:57:07 +0800

    Level5 initial

---
+++ Assets/Script/Level5/GirlHiking.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GirlHiking : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    [SerializeField] float Speed;
+    [SerializeField] Transform Dia1;
+    [SerializeField] Transform Dia2;
+    [SerializeField] Transform Dia3;
+    [SerializeField] Transform Dia4;
+    private float pointX;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        pointX = Dia1.position.x;
+    
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (transform.position.x > pointX)
+        {
+            rb.velocity = Vector2.zero;
+        }
+        else
+        {
+            rb.velocity = new Vector2(Speed, 0);
+        }
+    }
+}
+
