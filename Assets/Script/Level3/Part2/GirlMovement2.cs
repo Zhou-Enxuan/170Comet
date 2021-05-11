64d88b4ea32eb950389c7d6d239fc459abd384c6
BaoqingXie <39812037+BaoqingXie@users.noreply.github.com>
2021-05-12 05:15:56 +0800

    场景名，lv3part2

--- Assets/Script/Level3/Part2/GirlMovement2.cs
+++ Assets/Script/Level3/Part2/GirlMovement2.cs
@@ -8,9 +8,14 @@ public class GirlMovement2 : MonoBehaviour
     private float moveH;
     private Vector2 direction;
     [SerializeField] private float moveSpeed = 1.0f;
+    private Animator GirlAnimator;
+    private bool isnearWall;
+    public GameObject ClimbHint;
+    private bool FaceR;
     // Start is called before the first frame update
     void Awake(){
         rb = GetComponent<Rigidbody2D>();
+        GirlAnimator = GetComponent<Animator>();
     }
 
     void Start()
@@ -24,12 +29,66 @@ public class GirlMovement2 : MonoBehaviour
         if (GameManager.instance.stopMoving)
         {
             rb.velocity = Vector2.zero;
-        }
+        }else{
         moveH = Input.GetAxisRaw("Horizontal");
-        direction = new Vector2(moveH, 0);
-        rb.velocity = direction * moveSpeed;  
+        GirlAnimator.SetFloat("Direaction", moveH);
+        GirlAnimator.SetFloat("Speed", Mathf.Abs(moveH));
 
+        //Debug.Log("speed is " + Mathf.Abs(moveH));
+        //Debug.Log("direaction is " + moveH);
+
+        if(moveH < 0){
+            GirlAnimator.SetBool("FaceR", false);
+            //Debug.Log("FaceR is " + false);
+            FaceR = false;
+        }else if(moveH > 0){
+            GirlAnimator.SetBool("FaceR", true);
+            //Debug.Log("FaceR is " + true);
+            FaceR = true;
+        }
+        direction = new Vector2(moveH, 0);
+        rb.velocity = direction * moveSpeed;
+        }
+
+        if(isnearWall){
+            ClimbHint.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.Space) && FaceR){
+                GirlAnimator.SetBool("ClimbTrigger", true);
+                StartCoroutine(WaitAnimDone());
+            }
+        }else{
+            ClimbHint.SetActive(false);
+        }
     }
 
+    IEnumerator WaitAnimDone(){
+        yield return new WaitWhile(() => GirlAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime < 1);
+        GirlAnimator.SetBool("ClimbTrigger", false);
+        transform.position = new Vector3(26.84f, -1.69f, -0.6602975f);
+    }
 
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Wall")
+        {
+            isnearWall = true;
+            Debug.Log("Wall");
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Wall")
+        {
+            isnearWall = false;
+        }
+    }
+    
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Boundery")
+        {
+            LevelLoader.instance.LoadLevel("Level4");
+        }
+    }
 }
--- Assets/Script/Level3/Part2/LeaveRoom.cs
+++ Assets/Script/Level3/Part2/LeaveRoom.cs
@@ -5,35 +5,45 @@ using UnityEngine.SceneManagement;
 
 public class LeaveRoom : MonoBehaviour
 {
-    private bool IsinDoor = false ;
+    private bool IsinDoor = false;
+    private GameObject LeaveHint;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        LeaveHint  = GameObject.Find("LeaveHint");
+    }
+
+    void Start(){
+        LeaveHint.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(IsinDoor && Input.GetKeyDown(KeyCode.Space)){
+
+
+
+        if(IsinDoor && Input.GetKeyDown("space")){
             LevelLoader.instance.LoadLevel("Level3ClimbWall");
         }
     }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Door")
+        if (collision.gameObject.tag == "Player")
         {
             IsinDoor = true;
+            LeaveHint.SetActive(true);
         }
     }
 
     void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Door")
+        if (collision.gameObject.tag == "Player")
         {
             IsinDoor = false;
+            LeaveHint.SetActive(false);
         }
     }
 }
