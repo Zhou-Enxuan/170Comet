c8756fab9a2077c34e3826ca12e41882a6e07e1c
BaoqingXie <39812037+BaoqingXie@users.noreply.github.com>
2021-05-11 12:00:50 +0800

    part2

---
+++ Assets/Script/Level3/Part2/LeaveRoom.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LeaveRoom : MonoBehaviour
+{
+    private bool IsinDoor = false ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(IsinDoor && Input.GetKeyDown(KeyCode.Space)){
+            LevelLoader.instance.LoadLevel("Level3ClimbWall");
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Door")
+        {
+            IsinDoor = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Door")
+        {
+            IsinDoor = false;
+        }
+    }
+}
