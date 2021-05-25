29fd966fb3d9ba96e819560cc914f49aaa927c1f
Rui Chen <rchen47@ucsc.edu>
2021-05-25 21:27:33 +0800

    bug

--- Assets/Script/Level2/Fall/FallRoom.cs
+++ Assets/Script/Level2/Fall/FallRoom.cs
@@ -7,7 +7,7 @@ public class FallRoom : MonoBehaviour
 {
     public static GameObject QMark;
     public static GameObject Glass;
-    public static GameObject CatchUI;
+    // public static GameObject CatchUI;
     public static bool isRoomStart = false;
     bool isDiaActive = false;
 	private GameObject Hint;
@@ -16,13 +16,13 @@ public class FallRoom : MonoBehaviour
 		QMark = GameObject.Find("GirlQMark");
 		Glass = GameObject.Find("Glass");
 		Hint = GameObject.Find("Hint");
-		CatchUI = GameObject.Find("Fail");
+		// CatchUI = GameObject.Find("Fail");
 	}
 
 	void Start() {
 		Hint.SetActive(false);
 		Glass.SetActive(false);
-		CatchUI.SetActive(false);
+		// CatchUI.SetActive(false);
 		if (GameManager.instance.islv2FallGlassEnd) {
             isRoomStart = true;
 			QMark.SetActive(true);
@@ -33,11 +33,11 @@ public class FallRoom : MonoBehaviour
 	}
 
 	void Update(){
-		if (CatchUI.activeSelf && Hint.activeSelf) {
-			if (Input.GetKeyDown("space")) {
-				LevelLoader.instance.LoadLevel("Level3OpenWindow");
-			}
-		}
+		// if (CatchUI.activeSelf && Hint.activeSelf) {
+		// 	if (Input.GetKeyDown("space")) {
+		// 		LevelLoader.instance.LoadLevel("Level3OpenWindow");
+		// 	}
+		// }
 	}
 
 	void OnTriggerStay2D(Collider2D collision) {
@@ -68,8 +68,9 @@ public class FallRoom : MonoBehaviour
 	IEnumerator CheckDialogueDone()
     {
         yield return new WaitWhile(GameManager.instance.IsDialogShow);
-		CatchUI.SetActive(true);
-		Invoke("EndHint", 0.7f);
+		LevelLoader.instance.LoadLevel("Level3OpenWindow");
+		// CatchUI.SetActive(true);
+		// Invoke("EndHint", 0.7f);
 	}
 	void EndHint(){
 		Hint.SetActive(true);
