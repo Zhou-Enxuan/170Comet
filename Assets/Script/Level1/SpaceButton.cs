06c96c47bb197984aa4d56fec4c0e20e4eaaeb62
Rui Chen <rchen47@ucsc.edu>
2021-06-01 20:51:00 +0800

    space updated

---
+++ Assets/Script/Level1/SpaceButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpaceButton : MonoBehaviour
+{
+    [SerializeField] private Sprite ButtonImage;//普通的space
+    [SerializeField] private Sprite PressedImage;//按下的space
+
+    void Start()
+    {
+        GetComponent<Image>().sprite = ButtonImage;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("space")) {
+            GetComponent<Image>().sprite = PressedImage;
+        }else{
+            GetComponent<Image>().sprite = ButtonImage;
+        }
+    }
+}
