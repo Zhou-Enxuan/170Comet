5414f32da1b05f90095ea6601e75c9c674ad15b9
Rui Chen <rchen47@ucsc.edu>
2021-06-01 18:03:30 +0800

    update back home before win minigame

---
+++ Assets/Script/Level2/WrongRoom.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WrongRoom : MonoBehaviour
+{
+    private GameObject Hint;
+
+    void Awake()
+    {
+        Hint = GameObject.Find("Hint");
+    }
+
+    void Start()
+    {
+        Hint.SetActive(false);
+        if (!GameManager.instance.islv2SummerNewsEnd || !GameManager.instance.islv2FallGlassEnd) {
+            Dialog.PrintDialog("Lv2WithoutNews");//没拿报纸
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
